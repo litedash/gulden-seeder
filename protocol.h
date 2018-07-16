6af9f96590bf7514500983418c09a135142cd982
jerrybit <github@litedash.org>
2018-07-16 18:01:25 +0900

    change port

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 9923 : 9231;
+    return testnet ? 19753 : 9753;
 }
 
 //
